b3ba03d5800bb436b31dae4cda1e16b4fce0a66e
zamirova_s <zamirova_s@auca.kg>
2022-09-14 13:21:12 +0600

    third commit

---
+++ lab-01/src/Problem02.java
@@ -0,0 +1,7 @@
+public class Problem02 {
+    public static void main(String[] args) {
+        System.out.println("*********************");
+        System.out.println("*I like programming!*");
+        System.out.println("*********************");
+    }
+}
