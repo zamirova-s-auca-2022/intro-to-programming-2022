5d8ac520dde600509ff1304fd30c61d573254f46
zamirova_s <zamirova_s@auca.kg>
2022-09-22 15:53:49 +0600

    Addition Commit

--- lab-01/src/Problem06.java
+++ lab-01/src/Problem06.java
@@ -2,11 +2,12 @@ import java.util.jar.JarEntry;
 
 public class Problem06 {
     public static void main(String[] args) {
-        System.out.println(
-                   "    J      A     V     V   A \n" +
-                           "    J     A A     V   V   A A\n" +
-                           "J   J    AAAAA     V V   AAAAA\n" +
-                           " J J    A     A     V   A     A \n"
+        System.out.println("""
+                    J     A    V     V    A
+                    J    A A    V   V    A A
+                J   J   AAAAA    V V    AAAAA
+                 J J   A     A    V    A     A
+                """
         );
     }
 }
