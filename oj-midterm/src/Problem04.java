f20089cbab0c8f5eee18226ee4b424f6571cfa94
zamirova_s <zamirova_s@auca.kg>
2022-10-03 13:10:46 +0600

    Problem04 OJ-Midterm

---
+++ oj-midterm/src/Problem04.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Problem04 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double a = input.nextDouble();
+        double b = input.nextDouble();
+        double area = (a * b) / 2;
+        System.out.println(area);
+    }
+}
