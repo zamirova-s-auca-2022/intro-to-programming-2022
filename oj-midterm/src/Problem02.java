4edb0fd8bc035e5e9d437e787cd8defac04b1f18
zamirova_s <zamirova_s@auca.kg>
2022-10-03 13:02:26 +0600

    Problem02 OJ-Midterm

---
+++ oj-midterm/src/Problem02.java
@@ -0,0 +1,14 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Problem02 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+
+        int sum = a + b;
+
+        System.out.println(sum);
+    }
+}
