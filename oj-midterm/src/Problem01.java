b621dc41e2cb4e305d22b1f4cfaa40afa483116a
zamirova_s <zamirova_s@auca.kg>
2022-10-03 12:53:45 +0600

    Problem01 OJ-Midterm

---
+++ oj-midterm/src/Problem01.java
@@ -0,0 +1,10 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Problem01 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String word = input.next();
+        System.out.println(word + " " + word + " " + word);
+    }
+}
