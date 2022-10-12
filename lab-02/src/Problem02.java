22b7db31ddfb8cbce2e727da7dd3a2f98e10c569
zamirova_s <zamirova_s@auca.kg>
2022-10-12 14:48:31 +0600

    Problem02 lab-02

--- lab-02/src/Problem02.java
+++ lab-02/src/Problem02.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class Problem02 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("1st value? ");
+        int firstValue = input.nextInt();
+
+        System.out.print("2nd value? ");
+        int secondValue = input.nextInt();
+
+        System.out.printf("Before Swapping: a = %d, b = %d;%n ", firstValue, secondValue);
+
+        firstValue = firstValue + secondValue;
+        secondValue = firstValue - secondValue;
+        firstValue = firstValue - secondValue;
+
+        System.out.printf("After Swapping: a = %d, b = %d;%n", firstValue, secondValue);
+    }
 }
