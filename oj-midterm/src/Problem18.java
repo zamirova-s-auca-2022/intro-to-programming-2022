12a05f317460e26c57ee193f8402020e1e8f62d2
zamirova_s <zamirova_s@auca.kg>
2022-10-19 12:21:55 +0600

    OJ 18 Wrong Answer

---
+++ oj-midterm/src/Problem18.java
@@ -0,0 +1,23 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Problem18 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+        int d = input.nextInt();
+
+        double t = input.nextDouble();
+        double destination = Math.sqrt(Math.pow(c-a, 2) + Math.pow(d-b, 2));
+        double check = t - destination;
+
+        if (check > 0 &&  check % 2 == 0) {
+            System.out.println("y");
+        } else{
+            System.out.println("N");
+        }
+    }
+}
