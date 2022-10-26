5261717231077d796e1a26a8343678e3dc18f15d
zamirova_s <zamirova_s@auca.kg>
2022-10-26 14:51:11 +0600

    OJ 22 Second Wrong Answer

--- oj-midterm/src/Problem22.java
+++ oj-midterm/src/Problem22.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class Problem22 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int numberStones = input.nextInt();
-
+        int stoneLeft = input.nextInt();
+        int numberStones = stoneLeft + 2;
         if (numberStones % 2 == 0) {
             System.out.println("Bod");
         } else {
