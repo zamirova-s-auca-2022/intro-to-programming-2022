6bdc12395daa7e982b72983559d4018cd698591e
zamirova_s <zamirova_s@auca.kg>
2022-10-27 14:41:32 +0600

    OJ 22 Accepted

--- oj-midterm/src/Problem22.java
+++ oj-midterm/src/Problem22.java
@@ -4,11 +4,11 @@ public class Problem22 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int stoneLeft = input.nextInt();
-        int numberStones = stoneLeft + 2;
-        if (numberStones % 2 == 0) {
-            System.out.println("Bod");
-        } else {
+
+        if (stoneLeft % 2 == 1) {
             System.out.println("Alice");
+        } else {
+            System.out.println("Bob");
         }
     }
 }
