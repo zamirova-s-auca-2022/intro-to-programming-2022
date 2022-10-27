9ea247559feac4a287e9044bd3a1830e2f641b7a
zamirova_s <zamirova_s@auca.kg>
2022-10-27 14:25:55 +0600

    Problem06 lab-03

--- lab-03/src/Problem06.java
+++ lab-03/src/Problem06.java
@@ -4,20 +4,19 @@ public class Problem06 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a, b, c: ");
-
         double a = input.nextDouble();
         double b = input.nextDouble();
         double c = input.nextDouble();
-        double partOfDiscriminant = Math.pow(b, 2) - (4 * a * c);
-        double discriminant = Math.pow(partOfDiscriminant, 0.5);
-        double firstRoot = (-1 * b + discriminant) / 2 * a;
-        double secondRoot = (-1 * b - discriminant) / 2 * a;
 
-        if (discriminant > 0) {
-            System.out.printf("The equation has two roots %.6f and %.5f%n", firstRoot, secondRoot);
-        }
+        double discriminant = Math.pow(b, 2) - (4 * a * c);
+        double firstRoot = ((-1) * b + Math.pow(discriminant, 0.5)) / 2 * a;
+        double secondRoot = ((-1) * b - Math.pow(discriminant, 0.5)) / 2 * a;
+
         if (discriminant == 0){
-            System.out.printf("The equation has one root " + firstRoot);
+            System.out.println("The equation has one " + firstRoot);
+        }
+        if (discriminant > 0){
+            System.out.printf("The equation has two roots %.6f and %.5f%n", firstRoot, secondRoot);
         }
         if (discriminant < 0){
             System.out.println("The equation has no real roots");
