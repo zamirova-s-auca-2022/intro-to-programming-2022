41a161536f34eb4096bf9fde787ffe3381b0c8f1
zamirova_s <zamirova_s@auca.kg>
2022-10-27 17:54:17 +0600

    OJ 30 Accepted

--- oj-midterm/src/Problem30.java
+++ oj-midterm/src/Problem30.java
@@ -3,17 +3,18 @@ import java.util.Scanner;
 public class Problem30 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
         int a = input.nextInt();
         int b = input.nextInt();
         int c = input.nextInt();
 
-        if (a == 60 && b == 60 && c == 60){
+        if (a == 60 && b == 60 && c == 60) {
             System.out.println("Equilateral");
-        }else if (a + b + c == 180 && a == b || a == c || b == c){
+        } else if (a + b + c == 180 && a == b || a == c || b == c) {
             System.out.println("Isosceles");
-        }else  if (a + b + c == 180 ){
+        } else if (a + b + c == 180) {
             System.out.println("Scalene");
-        }else{
+        } else {
             System.out.println("Error");
         }
     }
